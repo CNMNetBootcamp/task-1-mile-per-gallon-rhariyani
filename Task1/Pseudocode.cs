3f9816c8d0d020ecadbc0f0573f43b1792285d35
Eliot Ostling <eostling@cnm.edu>
2019-01-22 18:09:39 -0700

    Graded: Please see TODO comments and resubmit

---
+++ Task1/Pseudocode.cs
@@ -0,0 +1,25 @@
+
+
+
+   // The Miles per Gallon Formula is:
+//(E-S)/G
+//Where:
+//S= Miles at the start of the tank(when filled first)
+//E= Miles at the end of the tank(when refilled)
+//G= Gallons used
+
+
+   // input
+    "Enter your starting Miles?"
+    "Enter Your ending Miles?"
+"Enter your Gallon used?"
+
+
+
+//process
+
+  Total Miles= E-S/G
+
+    //output
+
+  put  "Miles per Gallon is" + Total Miles
--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -6,7 +6,26 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //TODO:  For local variables they need to be lowercase
+            double Miles = 0;
+//input
+            Console.WriteLine("Please enter starting Miles:");
+            // TODO: Remove the var keyword and use the correct data type. Local variables should be lowercase
+            var SM = Single.Parse(Console.ReadLine());
+            // TODO: Remove the var keyword and use the correct data type. Local variables should be lowercase
+            Console.WriteLine("Please enter Ending Miles:");
+            var EM = Single.Parse(Console.ReadLine());
+            // TODO: Remove the var keyword and use the correct data type. Local variables should be lowercase
+            Console.WriteLine("Please enter Gasoline Used:");
+            var Gas = Single.Parse(Console.ReadLine());
+
+            //Process
+            //TODO: Calculation is incorrect
+            Miles = EM - SM / Gas;
+
+            //output    
+            Console.WriteLine("Total miles per gallon is " + Miles);
+            Console.ReadLine();
         }
     }
 }
